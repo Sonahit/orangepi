f302d623055ec9b9ae9cd99cb0efb00851b9a79b
Sonahit <grandpajok@gmail.com>
2023-09-10 23:43:38 +0600

    cmd

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -3,49 +3,50 @@ mod wiring_pi;
 use wiring_pi as wp;
 
 const PINS: wp::Pins = wp::Pins {
-    D0: wp::Pin {
+    D7: wp::Pin {
         mode: wp::LCDMode::OUTPUT,
-        index: 20,
-    },
-    D1: wp::Pin {
-        mode: wp::LCDMode::OUTPUT,
-        index: 19,
-    },
-    D2: wp::Pin {
-        mode: wp::LCDMode::OUTPUT,
-        index: 17,
-    },
-    D3: wp::Pin {
-        mode: wp::LCDMode::OUTPUT,
-        index: 3,
-    },
-    D4: wp::Pin {
-        mode: wp::LCDMode::OUTPUT,
-        index: 5,
-    },
-    D5: wp::Pin {
-        mode: wp::LCDMode::OUTPUT,
-        index: 2,
+        index: 0,
     },
     D6: wp::Pin {
         mode: wp::LCDMode::OUTPUT,
         index: 1,
     },
-    D7: wp::Pin {
+
+    D5: wp::Pin {
         mode: wp::LCDMode::OUTPUT,
-        index: 0,
+        index: 2,
+    },
+    D4: wp::Pin {
+        mode: wp::LCDMode::OUTPUT,
+        index: 5,
+    },
+    D3: wp::Pin {
+        mode: wp::LCDMode::OUTPUT,
+        index: 3,
+    },
+    D2: wp::Pin {
+        mode: wp::LCDMode::OUTPUT,
+        index: 17,
+    },
+    D1: wp::Pin {
+        mode: wp::LCDMode::OUTPUT,
+        index: 19,
+    },
+    D0: wp::Pin {
+        mode: wp::LCDMode::OUTPUT,
+        index: 20,
     },
     E: wp::Pin {
         mode: wp::LCDMode::OUTPUT,
         index: 22,
     },
-    RS: wp::Pin {
-        mode: wp::LCDMode::OUTPUT,
-        index: 25,
-    },
     RW: wp::Pin {
         mode: wp::LCDMode::OUTPUT,
         index: 23,
+    },
+    RS: wp::Pin {
+        mode: wp::LCDMode::OUTPUT,
+        index: 25,
     },
     is4PinMode: false,
 };
@@ -60,37 +61,42 @@ fn setup() {
 fn logic() {
     // https://www.sparkfun.com/datasheets/LCD/HD44780.pdf Table 12
     #[rustfmt::skip]
-    // wp::lcd_commands(
-    //     &[
-    //         0b00100000, 
-    //         0b00100000, 
-    //         0b00001110, 
-    //         0b00000110
-    //     ]
-    // );
+    wp::lcd_commands(
+        &[
+            0b00110000, 
+            0b00001110, 
+            0b00000110
+        ]
+    );
 
-    // wp::lcd_string("he");
+    wp::lcd_string("Hitachi");
+
+    wp::lcd_command(0b00000111);
+
+    wp::lcd_string(" Mikrocom");
+
+    wp::lcd_command(0b10);
 
     // 2 lines, 5x7 format
     // wp::lcd_command(0b00111000);
 
     // display on
-    wp::lcd_command(0b00001110);
+    // wp::lcd_command(0b00001110);
 
     // clear display (optional here)
-    wp::lcd_command(0x01);
+    // wp::lcd_command(0x01);
 
     // first line
     // entrymode
-    wp::lcd_command(0b00000110);
+    // wp::lcd_command(0b00000110);
 
-    wp::lcd_string("Hitachi");
+    // wp::lcd_string("Hitachi");
     //nextline
-    wp::lcd_command(0b11000000);
-    wp::lcd_string("MICROCOM");
+    // wp::lcd_command(0b11000000);
+    // wp::lcd_string("MICROCOM");
 
     // HOME
-    wp::lcd_command(0b10);
+    // wp::lcd_command(0b10);
 }
 
 fn main() {
