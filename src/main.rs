2f65b4dcb3627cf8b48ebf4e8785021d255987b6
Sonahit <grandpajok@gmail.com>
2023-09-15 20:29:23 +0600

    two

--- src/main.rs
+++ src/main.rs
@@ -126,14 +126,14 @@ fn logic(port: I2CPort) {
     let padding = Padding(port.width() as usize);
     loop {
         // port.lcd_string_u8(padding.right_pad_u8(&[0b11110100], "<").as_slice(), LINE_1);
-        port.lcd_string(padding.left_pad("World", ">"), Lines::None);
+        port.lcd_string(padding.left_pad("World", ">"), Lines::Two);
 
         thread::sleep(time::Duration::from_millis(1000));
 
         // port.lcd_string(padding.right_pad("World", "<"), LINE_1);
         port.lcd_string_u8(
             padding.left_pad_u8(&[0b11110100], "<").as_slice(),
-            Lines::None,
+            Lines::Two,
         );
         thread::sleep(time::Duration::from_millis(1000));
         println!("Loop done")
