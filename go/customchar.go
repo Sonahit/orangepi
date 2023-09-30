4c454bad0c338b92480141105059817f384bdc82
Sonahit <grandpajok@gmail.com>
2023-09-30 23:36:41 +0600

    go wtf slices

--- go/customchar.go
+++ go/customchar.go
@@ -1,14 +1,22 @@
 package main
 
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
 // byte customChar[8] = {
-// 	0b11111,
-// 	0b11111,
-// 	0b11111,
-// 	0b11111,
-// 	0b11111,
-// 	0b11111,
-// 	0b11111,
-// 	0b11111
+// 	0b100000000001,
+// 	0b111111111111,
+// 	0b000000000000,
+// 	0b111111111111,
+// 	0b111111111111,
+// 	0b111111111111,
+// 	0b000000000000,
+// 	0b111111111111
 // };
 
 // Чтобы сделать custom-char
@@ -18,6 +26,63 @@ type CustomChar struct {
 	Rows []int
 }
 
+const (
+	CHAR_HEIGHT = 8
+	CHAR_LENGTH = 5
+)
+
 func NewCustomChar(rows []int) CustomChar {
-	return CustomChar{Rows: rows}
+	return CustomChar{
+		Rows: rows,
+	}
+}
+
+func printRows(rows []int) {
+	for height := 0; height < len(rows); height += 1 {
+		log.Println(strconv.FormatInt(int64(rows[height]), 2))
+	}
+}
+
+func maxRowsLength(rows []int) int {
+	return slices.MaxFunc[[]int](rows, func(a, b int) int {
+		return strings.Compare(strconv.FormatInt(int64(a), 2), strconv.FormatInt(int64(b), 2))
+	})
+}
+
+func rowsZero(rows []int) bool {
+	return slices.Max[[]int](rows) == 0
+}
+
+func (ch CustomChar) SplitBySections() (sectionsNumber int, locations [][]int) {
+	copyMatrix := make([][]int, 0, len(ch.Rows))
+	if len(ch.Rows) > CHAR_HEIGHT {
+		copyMatrix = append(copyMatrix, ch.Rows[:CHAR_HEIGHT], ch.Rows[CHAR_HEIGHT:len(ch.Rows)])
+	} else {
+		copyMatrix = append(copyMatrix, ch.Rows[:len(ch.Rows)])
+	}
+
+	for _, rows := range copyMatrix {
+		for {
+			if rowsZero(rows) {
+				break
+			}
+			newRows := make([]int, len(rows))
+			// firstChar from left
+			// LE
+			for height := 0; height < len(rows); height += 1 {
+				row := 0
+				if rows[height] != 0 {
+					for length := 0; length < CHAR_LENGTH; length += 1 {
+						bit := rows[height] >> length % 2
+						row |= bit << length
+					}
+					rows[height] >>= CHAR_LENGTH
+				}
+				newRows[height] = int(row)
+			}
+			locations = append(locations, newRows)
+		}
+	}
+	sectionsNumber = len(locations)
+	return sectionsNumber, locations
 }
---
+++ go/customchar_test.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestWithin8Chars(t *testing.T) {
+	ch := NewCustomChar([]int{
+		0b00001,
+		0b00011,
+		0b00111,
+		0b01111,
+		0b11111,
+	})
+
+	_, splitted := ch.SplitBySections()
+
+	if splitted[0][4] != 0b11111 {
+		t.Fatalf("Expected %d, got %d", 0b11111, splitted[0][4])
+	}
+}
+
+func TestWithin10Chars(t *testing.T) {
+	ch := NewCustomChar([]int{
+		0b1001100001,
+		0b1011000011,
+		0b1011000111,
+		0b1111001111,
+		0b1000011111,
+	})
+	_, splitted := ch.SplitBySections()
+
+	if splitted[0][4] != 0b11111 {
+		t.Fatalf("Expected %d, got %d", 0b11111, splitted[0][4])
+	}
+	if splitted[1][0] != 0b10011 {
+		t.Fatalf("Expected %d, got %d", 0b10011, splitted[1][0])
+	}
+}
+
+func TestWithin11Chars(t *testing.T) {
+	ch := NewCustomChar([]int{
+		0b11001100001,
+		0b11011000011,
+		0b11011000111,
+		0b11111001111,
+		0b11000011111,
+	})
+
+	_, splitted := ch.SplitBySections()
+	if splitted[2][0] != 0b1 {
+		t.Fatalf("Expected %d, got %d", 0b1, splitted[2][0])
+	}
+}
+
+func TestWithin11Chars2Height(t *testing.T) {
+	ch := NewCustomChar([]int{
+		0b00001,
+		0b00011,
+		0b00111,
+		0b01111,
+		0b11111,
+		0b00001,
+		0b00011,
+		0b00111,
+		0b11111,
+	})
+
+	_, splitted := ch.SplitBySections()
+	if splitted[1][0] != 0b11111 {
+		t.Fatalf("Expected %d, got %d", 0b11111, splitted[0][7])
+	}
+}
+
+func TestSymbol(t *testing.T) {
+	ch := NewCustomChar([]int{
+		0b000111111110000,
+		0b001000010001000,
+		0b011000010000100,
+		0b010100010001010,
+		0b010010010010010,
+		0b010001010100010,
+		0b010000111000010,
+		0b011111111111110,
+		0b010000111000010,
+		0b010001010100010,
+		0b010010010010010,
+		0b010010010010010,
+		0b010010010001010,
+		0b010100010000100,
+		0b001000010001000,
+		0b000111111110000,
+	})
+
+	_, splitted := ch.SplitBySections()
+
+	if splitted[0][0] != 0b10000 {
+		t.Fatalf("Expected %d, got %d", 0b10000, splitted[0][0])
+	}
+	if splitted[2][0] != 0b11 {
+		t.Fatalf("Expected %d, got %d", 0b11, splitted[2][0])
+	}
+	if splitted[3][7] != 0b10000 {
+		t.Fatalf("Expected %d, got %d", 0b10000, splitted[3][7])
+	}
+}
+
+func TestSymbol100000Times(t *testing.T) {
+	ch := NewCustomChar([]int{
+		0b000111111110000,
+		0b001000010001000,
+		0b011000010000100,
+		0b010100010001010,
+		0b010010010010010,
+		0b010001010100010,
+		0b010000111000010,
+		0b011111111111110,
+		0b010000111000010,
+		0b010001010100010,
+		0b010010010010010,
+		0b010010010010010,
+		0b010010010001010,
+		0b010100010000100,
+		0b001000010001000,
+		0b000111111110000,
+	})
+	for i := 0; i < 100000; i++ {
+		slicesNum, splitted := ch.SplitBySections()
+		if slicesNum == 0 {
+			t.Fatal("Expected more than 0 slices")
+		}
+		printRows(splitted[0])
+		if splitted[0][0] != 0b10000 {
+			t.Fatalf("Expected %d, got %d", 0b10000, splitted[0][0])
+		}
+		if splitted[2][0] != 0b11 {
+			t.Fatalf("Expected %d, got %d", 0b11, splitted[2][0])
+		}
+		if splitted[3][7] != 0b10000 {
+			t.Fatalf("Expected %d, got %d", 0b10000, splitted[3][7])
+		}
+	}
+}
