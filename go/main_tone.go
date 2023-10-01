6c4a1bf28bb575bbd7f0919e4a735f1d6ea2ef01
Sonahit <grandpajok@gmail.com>
2023-10-01 19:00:49 +0600

    tone

--- go/main_tone.go
+++ go/main_tone.go
@@ -13,7 +13,12 @@ func mainTone() {
 
 	buzzer := NewBuzzerPwm(PIN_WPI)
 	buzzer.ChangeHz(1000)
-
+	scale := [8]int{262, 294, 330, 349, 392, 440, 494, 525}
 	for {
+		for _, s := range scale {
+			log.Printf("%d\n", s)
+			buzzer.ChangeHz(s)
+			sleep(500)
+		}
 	}
 }
