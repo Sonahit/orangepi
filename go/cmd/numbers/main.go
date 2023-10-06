0b4c5f9d23a96a8fafa69cfd9724eca62c72191c
Sonahit <grandpajok@gmail.com>
2023-10-06 20:28:21 +0600

    g

--- go/cmd/numbers/main.go
+++ go/cmd/numbers/main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"display/lib"
-	"display/pkg"
 	"log"
 )
 
+// |   19 |  17 |    SDA.1 | ALT3 | 0 | 27 || 28 | 1 | OUT  | SCL.1    | 18  | 18   |
+// |    7 |  19 |     PA07 |  OFF | 0 | 29 || 30 |   |      | GND      |     |      |
+// |    8 |  20 |     PA08 |  OFF | 0 | 31 || 32 | 1 | OUT  | RTS.1    | 21  | 200  |
+// |    9 |  22 |     PA09 |  OFF | 0 | 33 || 34 |   |      | GND      |     |      |
+// |   10 |  23 |     PA10 |  OFF | 0 | 35 || 36 | 1 | OUT  | CTS.1    | 24  | 201  |
+// |   20 |  25 |     PA20 |  OFF | 0 | 37 || 38 | 1 | OUT  | TXD.1    | 26  | 198  |
+// |      |     |      GND |      |   | 39 || 40 | 0 | OUT  | RXD.1    | 27  | 199  |
+// +------+-----+----------+------+---+----++----+---+------+----------+-----+------+
+
 const (
-	PIN_0 = 25
-	PIN_1 = 23
-	PIN_2 = 22
-	PIN_3 = 20
-	PIN_4 = 19
-	PIN_5 = 27
-	PIN_6 = 26
-	PIN_7 = 24
-	PIN_8 = 21
-	PIN_9 = 18
+	PIN_0 = 25 // 6 b
+	PIN_1 = 23 // 7 a
+	PIN_2 = 22 // 8 DP
+	PIN_3 = 20 // 9 f
+	PIN_4 = 19 // 10 g
+	PIN_5 = 27 // 5 GREEN
+	PIN_6 = 26 // 4 c
+	PIN_7 = 24 // 3 d
+	PIN_8 = 21 // 2 e
+	PIN_9 = 18 // 1 RED
 )
 
 const (
@@ -64,12 +72,8 @@ func logicNumber() {
 	lib.DigitalWrite(RED_PIN, lib.DIGITAL_HIGH)
 	lib.DigitalWrite(GREEN_PIN, lib.DIGITAL_HIGH)
 	lib.DigitalWrite(DP_PIN, lib.DIGITAL_HIGH)
-	for {
-		for _, pin := range DATA_PINS {
-			toggle(pin)
-			pkg.Sleep(200)
-		}
-	}
+
+	lib.DigitalWrite(F_PIN, lib.DIGITAL_HIGH)
 }
 
 var toggled map[int]bool = make(map[int]bool)
