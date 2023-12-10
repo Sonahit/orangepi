34e7677e6f87bfe944a0d93f39019b77204b83b0
Sonahit <grandpajok@gmail.com>
2023-12-10 15:23:29 +0600

    pwm

---
+++ go/cmd/softpwm/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"display/lib"
+	"display/pkg"
+	"flag"
+	"log"
+)
+
+func main() {
+	var pin int
+	flag.IntVar(&pin, "pin", 2, "pin pwm")
+	flag.Parse()
+
+	speed := 0
+	rng := 10
+
+	if err := lib.WiringSetup(); err != 0 {
+		log.Fatalf("Setup error %d", err)
+	}
+	defer lib.PwmStop(pin)
+	lib.PinMode(pin, lib.PIN_OUTPUT)
+	lib.PwmCreate(pin, speed, rng)
+
+	for {
+		if speed == rng {
+			speed = 0
+		} else {
+			speed += 1
+		}
+
+		lib.PwmWrite(pin, speed)
+		pkg.Sleep(5000)
+	}
+}
